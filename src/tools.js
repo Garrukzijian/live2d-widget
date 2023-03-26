92d49bd8c8c2f5e18c04dfc6e8d28edeba60972c
Zijian Zhou <86515498+Garrukzijian@users.noreply.github.com>
2023-03-27 01:29:25 +0700

    1

--- src/tools.js
+++ src/tools.js
@@ -13,7 +13,7 @@ function showHitokoto() {
     fetch("https://v1.hitokoto.cn")
         .then(response => response.json())
         .then(result => {
-            const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
+            const text = `This come form <span>「${result.from}」</span>，是 <span>${result.creator}</span> push on hitokoto.cn。`;
             showMessage(result.hitokoto, 6000, 9);
             setTimeout(() => {
                 showMessage(text, 4000, 9);
@@ -50,7 +50,7 @@ const tools = {
     "photo": {
         icon: fa_camera_retro,
         callback: () => {
-            showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
+            showMessage("Am I look pretty?", 6000, 9);
             Live2D.captureName = "photo.png";
             Live2D.captureFrame = true;
         }
@@ -65,7 +65,7 @@ const tools = {
         icon: fa_xmark,
         callback: () => {
             localStorage.setItem("waifu-display", Date.now());
-            showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
+            showMessage("May you reunite with important people someday.", 2000, 11);
             document.getElementById("waifu").style.bottom = "-500px";
             setTimeout(() => {
                 document.getElementById("waifu").style.display = "none";
